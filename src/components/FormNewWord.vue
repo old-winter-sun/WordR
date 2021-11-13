321801af731906e360cfdee7fc1ffcfa3fc7850c
winter-sun <65158405+old-winter-sun@users.noreply.github.com>
2021-11-13 16:18:26 +0800

    初具规模

---
+++ src/components/FormNewWord.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="left">
+    <input type="search" />
+    <input type="submit" />
+    <li v-for="entry in dict" :key="entry.id">
+      {{ entry.value }}
+    </li>
+  </div>
+  <div id="right"><textarea>翻译放这儿</textarea></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dict: [
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
+// const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, './dist'),
+// base: path.resolve(__dirname, './dist'),
   plugins: [vue()]
 })
